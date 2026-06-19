b1bad05e725c0367d0136d49482653a59fa5b518
Srikar <kannayarao1@gmail.com>
2026-06-19 14:27:45 +0530

    Frontend ready for Vercel

---
+++ frontend/src/services/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://fixiq-production-836c.up.railway.app",
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default API;
